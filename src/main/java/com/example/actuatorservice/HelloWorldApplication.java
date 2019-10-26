19fe9b49d6c739cc01ef09686d5199a3153cb64e
Enrique Coello <enrique.coello@gmail.com>
2019-10-26 16:03:45 -0400

    Updated main class to include components from other packages, besides default package

--- src/main/java/com/example/actuatorservice/HelloWorldApplication.java
+++ src/main/java/com/example/actuatorservice/HelloWorldApplication.java
@@ -2,7 +2,9 @@ package com.example.actuatorservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan({"com.example.actuatorservice", "com.example.customendpoint"})
 @SpringBootApplication
 public class HelloWorldApplication {
     public static void main(String[] args) {
